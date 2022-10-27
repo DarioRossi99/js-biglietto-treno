06658bacedadf124b04b161c1fe92e1a1dcdafb4
DarioRossi99 <rssdra99@gmail.com>
2022-10-27 17:09:02 +0200

    aggiunto toFixed

--- biglietti.js/main.js
+++ biglietti.js/main.js
@@ -7,21 +7,20 @@ let priceJourney = .21 * kilometriViaggio
 
 
 if (etaSoggetto < 18){
-    alert(priceJourney * .80);
-}else if(etaSoggetto >= 18 && etaSoggetto <= 65){
-   alert(priceJourney); 
-}else if(etaSoggetto > 65){
+    alert(priceJourney * .80); 
+}else if (etaSoggetto > 65){
     alert(priceJourney * .60);
+}else{
+    alert(priceJourney);
 }
 
 if (etaSoggetto < 18){
     upDateUtente = document.getElementById("prezzoBiglietto");
-    upDateUtente.innerHTML = priceJourney * .80;
-}else if(etaSoggetto >= 18 && etaSoggetto <= 65){
+    upDateUtente.innerHTML = (priceJourney * .80).toFixed(2);
+}else if (etaSoggetto > 65){
+    upDateUtente = document.getElementById("prezzoBiglietto");
+    upDateUtente.innerHTML = (priceJourney * .60).toFixed(2);
+}else {
     upDateUtente = document.getElementById("prezzoBiglietto");
     upDateUtente.innerHTML = priceJourney;
-}else if(etaSoggetto > 65){
-    upDateUtente = document.getElementById("prezzoBiglietto");
-    upDateUtente.innerHTML = priceJourney * .60
 }
-
