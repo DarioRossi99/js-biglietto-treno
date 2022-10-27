d86361d32acfe8a3bf432cd960913f73691bbd2f
DarioRossi99 <rssdra99@gmail.com>
2022-10-27 16:22:38 +0200

    correzione if

--- biglietti.js/main.js
+++ biglietti.js/main.js
@@ -10,17 +10,18 @@ if (etaSoggetto < 18){
     alert(priceJourney * .80);
 }else if(etaSoggetto >= 18 && etaSoggetto <= 65){
    alert(priceJourney); 
-}if(etaSoggetto > 65){
+}else if(etaSoggetto > 65){
     alert(priceJourney * .60);
 }
 
 if (etaSoggetto < 18){
     upDateUtente = document.getElementById("prezzoBiglietto");
-    upDateUtente.innerHTML = priceJourney * .80
+    upDateUtente.innerHTML = priceJourney * .80;
 }else if(etaSoggetto >= 18 && etaSoggetto <= 65){
     upDateUtente = document.getElementById("prezzoBiglietto");
     upDateUtente.innerHTML = priceJourney;
-}if(etaSoggetto > 65){
+}else if(etaSoggetto > 65){
     upDateUtente = document.getElementById("prezzoBiglietto");
     upDateUtente.innerHTML = priceJourney * .60
 }
+
